33df12dc032ab366d2e6b75621c1e6a87e02ad5f
yara <uraeel@gmail.com>
2024-06-18 17:05:21 -0400

    test upload image for github

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// server/app.js
-
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const authRoutes = require('./routes/authRoutes');
-const postRoutes = require('./routes/postRoutes');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+const uploadRoutes = require("./routes/uploadRoutes"); // Import upload routes
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 
@@ -16,23 +15,24 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
-      'https://yarazarin.github.io'
+      "http://localhost:3000",
+      "https://yarazarin.github.io"
     ],
   })
 );
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB connection error:', err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
-app.use('/auth', authRoutes); // Mount authRoutes correctly
-app.use('/posts', postRoutes);
+app.use("/auth", authRoutes);
+app.use("/posts", postRoutes);
+app.use("/upload", uploadRoutes); // Add upload route
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
