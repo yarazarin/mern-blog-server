33ed4a7b04a88b13cabc7e76596069d1a9be8eaf
yara <uraeel@gmail.com>
2024-06-18 17:51:21 -0400

    ret

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const path = require("path");
-const authRoutes = require("./routes/authRoutes");
-const postRoutes = require("./routes/postRoutes");
-const uploadRoutes = require("./routes/uploadRoutes"); // Import upload routes
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+// server/app.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const authRoutes = require('./routes/authRoutes');
+const postRoutes = require('./routes/postRoutes');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const app = express();
 
@@ -15,25 +16,23 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "https://yarazarin.github.io",
-      "https://github.com/yarazarin/mern-blog-server/upload"
+      'http://localhost:3000',
+      'https://yarazarin.github.io'
     ],
   })
 );
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'));
 
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
 
-app.use("/auth", authRoutes);
-app.use("/posts", postRoutes);
-app.use("/upload", uploadRoutes); // Add upload route
+app.use('/auth', authRoutes); // Mount authRoutes correctly
+app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
