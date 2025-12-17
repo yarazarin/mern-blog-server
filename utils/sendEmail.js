4b723e1387b8df3270d80f7d93135cd2180df8e9
yara <itisyara@gmail.com>
2025-12-17 16:20:44 -0500

    Fix typo in nodemailer transport creation method

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
